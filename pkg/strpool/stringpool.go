ae3fe9b9d17e78f3dbc52bc56ddb493d58ddfb4a
Clayton O'Neill <claytono@github.com>
2018-12-13 12:43:30 -0500

    Implementation based on rwmutex

--- pkg/strpool/stringpool.go
+++ pkg/strpool/stringpool.go
@@ -6,22 +6,36 @@ import "sync"
 // to be shared across an instance of the thanos store.  This is mostly used to
 // reduce the size of index cache references.
 type StringPool struct {
-	pool           *sync.Map
+	rwm            *sync.RWMutex
+	pool           map[string]string
 	bytesStored    int64
 	bytesRetrieved int64
 }
 
 // NewStringPool seturns a new StringPool instance
 func NewStringPool() *StringPool {
-	return &StringPool{pool: &sync.Map{}}
+	return &StringPool{
+		rwm:  &sync.RWMutex{},
+		pool: map[string]string{},
+	}
 }
 
-// GetCachedString returns a new copy of the string that is shared
+// GetCachedString returns a copy of the string that is shared if it is already
+// in the pool, otherwise it returns the original string and stores it in the
+// pool.
 func (sc *StringPool) GetCachedString(s string) (cs string, ok bool) {
-	iface, ok := sc.pool.LoadOrStore(s, s)
 	sc.bytesRetrieved += int64(len(s))
-	if !ok {
-		sc.bytesStored += int64(len(s))
+
+	sc.rwm.RLock()
+	cs, ok = sc.pool[s]
+	sc.rwm.RUnlock()
+	if ok {
+		return cs, ok
 	}
-	return iface.(string), ok
+
+	sc.bytesRetrieved += int64(len(s))
+	sc.rwm.Lock()
+	sc.pool[s] = s
+	sc.rwm.Unlock()
+	return s, ok
 }
